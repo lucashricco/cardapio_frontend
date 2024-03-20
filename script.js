f884587f56d65c8274c74e634919f03e14c5ce06
lucashricco <silianolucas@gmail.com>
2024-03-20 19:28:42 -0300

    inicio da programação das funções do site

---
+++ script.js
@@ -0,0 +1,35 @@
+
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItemsContainer = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const cartCounter = document.getElementById("cart-count")
+const addressInput = document.getElementById("address")
+const addressWarn = document.getElementById("address-warn")
+
+
+//chamando o modal de informações ao clicar no botao do carrinho
+cartBtn.addEventListener("click",function(){
+    cartModal.style.display = "flex"
+})
+
+//fechar o modal quando clicar fora ou no botao fechar
+cartModal.addEventListener("click",function(event){
+    if(event.target === cartModal){
+        cartModal.style.display = "none"
+    }
+})
+
+closeModalBtn.addEventListener("click",function(){
+    cartModal.style.display = "none"
+})
+
+//add itens no carrinho
+menu.addEventListener("click", function(event){
+
+    let ParentButton = event.target.closest(".add-to-cart-btn")
+    
+})
